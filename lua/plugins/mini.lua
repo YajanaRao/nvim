de79d811d0f3268ba68d0ed0f9ffe07adae89c15
Yajana Rao <yajanarao@Yajanas-MacBook-Air.local>
2025-10-11 20:28:39 +0530

    mini.move: map Alt-hjkl for moving selections/lines

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -40,7 +40,18 @@ return {
       require('mini.pairs').setup()
       require('mini.bracketed').setup()
       require('mini.tabline').setup()
-      require('mini.move').setup()
+      require('mini.move').setup({
+        mappings = {
+          left = '<M-h>',
+            right = '<M-l>',
+            down = '<M-j>',
+            up = '<M-k>',
+            line_left = '<M-h>',
+            line_right = '<M-l>',
+            line_down = '<M-j>',
+            line_up = '<M-k>',
+        }
+      })
 
       local hipatterns = require 'mini.hipatterns'
 
